ce8ba031cc7858c46e8d51db084fb61cb893f2e6
hemantic <artem.kiselev@gmail.com>
2019-12-31 01:12:54 +0300

    oh my

--- wildsearch_crawler/spiders/wildberries_categories_spider.py
+++ wildsearch_crawler/spiders/wildberries_categories_spider.py
@@ -5,9 +5,6 @@ import datetime
 import scrapy
 import requests
 
-from scrapy.utils import iterators
-from scrapy.loader import ItemLoader
-from wildsearch_crawler.items import WildsearchCrawlerItemWildberriesCategory
 
 class WildberriesCategoriesSpider(scrapy.Spider):
     name = "wb_categories"
@@ -15,17 +12,6 @@ class WildberriesCategoriesSpider(scrapy.Spider):
 
     def parse(self, response):
         for url in response.css('#sitemap a'):
-            #category = WildsearchCrawlerItemWildberriesCategory()
-            #loader = ItemLoader(item=category, response=response)
-
-            #loader.add_value('parse_date', datetime.datetime.now().isoformat(" "))
-            #loader.add_value('marketplace', 'wildberries')
-
-            #loader.add_value('wb_category_name', url.css('::text').get())
-            #loader.add_value('wb_category_url', url.attrib['href'])
-            #loader.add_value('wb_category_level', None)
-
-            #yield loader.load_item()
             yield {
                 'parse_date': datetime.datetime.now().isoformat(" "),
                 'marketplace': 'wildberries',
