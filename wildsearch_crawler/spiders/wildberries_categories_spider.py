c89561e9d65ad6e551bda9a7acd9dadf1d750f18
hemantic <artem.kiselev@gmail.com>
2019-12-31 01:05:34 +0300

    Some optimizations for wildberries category parser

--- wildsearch_crawler/spiders/wildberries_categories_spider.py
+++ wildsearch_crawler/spiders/wildberries_categories_spider.py
@@ -3,11 +3,9 @@
 import datetime
 
 import scrapy
-import re
-import logging
 import requests
-import urllib.parse
 
+from scrapy.utils import iterators
 from scrapy.loader import ItemLoader
 from wildsearch_crawler.items import WildsearchCrawlerItemWildberriesCategory
 
@@ -17,17 +15,23 @@ class WildberriesCategoriesSpider(scrapy.Spider):
 
     def parse(self, response):
         for url in response.css('#sitemap a'):
-            category = WildsearchCrawlerItemWildberriesCategory()
-            loader = ItemLoader(item=category, response=response)
+            #category = WildsearchCrawlerItemWildberriesCategory()
+            #loader = ItemLoader(item=category, response=response)
 
-            loader.add_value('parse_date', datetime.datetime.now().isoformat(" "))
-            loader.add_value('marketplace', 'wildberries')
+            #loader.add_value('parse_date', datetime.datetime.now().isoformat(" "))
+            #loader.add_value('marketplace', 'wildberries')
 
-            loader.add_value('wb_category_name', url.css('::text').get())
-            loader.add_value('wb_category_url', url.attrib['href'])
-            loader.add_value('wb_category_level', None)
+            #loader.add_value('wb_category_name', url.css('::text').get())
+            #loader.add_value('wb_category_url', url.attrib['href'])
+            #loader.add_value('wb_category_level', None)
 
-            yield loader.load_item()
+            #yield loader.load_item()
+            yield {
+                'parse_date': datetime.datetime.now().isoformat(" "),
+                'marketplace': 'wildberries',
+                'wb_category_name': url.css('::text').get(),
+                'wb_category_url': url.attrib['href']
+            }
 
     def closed(self, reason):
         callback_url = getattr(self, 'callback_url', None)
