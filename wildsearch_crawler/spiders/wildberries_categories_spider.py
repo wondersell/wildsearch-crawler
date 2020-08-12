a7c40d9ad5822b60ff5a9c9521d892069887c6b4
hemantic <artem.kiselev@gmail.com>
2020-08-13 02:26:44 +0300

    Wildberries category parser updated

--- wildsearch_crawler/spiders/wildberries_categories_spider.py
+++ wildsearch_crawler/spiders/wildberries_categories_spider.py
@@ -1,5 +1,6 @@
 import datetime
 import logging
+import scrapy
 
 from .base_spider import BaseSpider
 from urllib.parse import urlparse, urljoin
@@ -9,18 +10,20 @@ logger = logging.getLogger(__name__)
 
 class WildberriesCategoriesSpider(BaseSpider):
     name = "wb_categories"
-    start_urls = ['https://www.wildberries.ru/services/karta-sayta']
 
-    def parse(self, response):
-        start_url_parsed = urlparse(response.request.url)
+    def start_requests(self):
+        yield scrapy.Request('https://www.wildberries.ru/menu/getrendered?lang=ru&burger=true', self.parse_main_menu, headers={'x-requested-with': 'XMLHttpRequest'})
 
-        for url in response.css('#sitemap a'):
-            url_parsed = urlparse(url.attrib['href'])
-            full_url = urljoin(start_url_parsed.scheme + '://' + start_url_parsed.netloc, url_parsed.path)
+    def parse_main_menu(self, response):
+        # start_url_parsed = urlparse(response.request.url)
+
+        for url in response.css('a'):
+            #url_parsed = urlparse(url.attrib['href'])
+            #full_url = urljoin(start_url_parsed.scheme + '://' + start_url_parsed.netloc, url_parsed.path)
 
             yield {
                 'parse_date': datetime.datetime.now().isoformat(" "),
                 'marketplace': 'wildberries',
                 'wb_category_name': url.css('::text').get(),
-                'wb_category_url': full_url
+                'wb_category_url': url.attrib['href']
             }
