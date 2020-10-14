592e2a122d3f5bc8b42521550d9f4873b9762115
hemantic <artem.kiselev@gmail.com>
2020-10-14 22:02:11 +0300

    Added rotating proxy download middleware

--- wildsearch_crawler/settings.py
+++ wildsearch_crawler/settings.py
@@ -52,9 +52,11 @@ ROBOTSTXT_OBEY = False
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'wildsearch_crawler.middlewares.WildsearchCrawlerDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'wildsearch_crawler.middlewares.WildsearchCrawlerDownloaderMiddleware': 543,
+    'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,
+    'rotating_proxies.middlewares.BanDetectionMiddleware': 620,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
