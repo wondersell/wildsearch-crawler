716987f489e736d53b63a97c18f8f37b1217888d
hemantic <artem.kiselev@gmail.com>
2020-10-22 03:05:31 +0300

    Download middlewares switched on

--- wildsearch_crawler/settings.py
+++ wildsearch_crawler/settings.py
@@ -52,10 +52,10 @@ ROBOTSTXT_OBEY = False
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'wildsearch_crawler.middlewares.RotatingProxyMiddleware': 610,
-#    'wildsearch_crawler.middlewares.BanDetectionMiddleware': 620,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'wildsearch_crawler.middlewares.RotatingProxyMiddleware': 610,
+    'wildsearch_crawler.middlewares.BanDetectionMiddleware': 620,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
